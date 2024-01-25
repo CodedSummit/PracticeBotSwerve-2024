6e19ae83fd438e8786993c2280ea657dfebbaf7b
Cliff Stockdill <cstockdill@yahoo.com>
2024-01-25 12:43:53 -0700

    Add timeout to tag chasing

--- src/main/java/frc/robot/commands/ChaseTagCommand.java
+++ src/main/java/frc/robot/commands/ChaseTagCommand.java
@@ -17,6 +17,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -40,6 +41,8 @@ public class ChaseTagCommand extends Command {
   public static final Transform2d CAMERA_TO_ROBOT = 
         new Transform2d(new Translation2d(inchesToMeters(12.75), 0.0), new Rotation2d(0.0));
 
+  private static final double STALE_TARGET_TIME = 2.0;
+
   private final VisionSubsystem m_VisionSubsystem;
   private final SwerveSubsystem drivetrainSubsystem;
   
@@ -51,8 +54,13 @@ public class ChaseTagCommand extends Command {
   private PhotonTrackedTarget lastTarget;
   private  DoublePublisher m_xVelPub;
   private  DoublePublisher m_yVelPub;
+  private  DoublePublisher m_xPub;
+  private  DoublePublisher m_yPub;
+  private  DoublePublisher m_omegaPub;
+
   private double m_xRobotPose;
   private double m_yRobotPose;
+  private Timer m_TargetLastSeen = null;
 
   public ChaseTagCommand(
         VisionSubsystem visionSubsystem, 
@@ -80,6 +88,7 @@ public class ChaseTagCommand extends Command {
     yController.reset(robotPose.getY());
     setupPublishers();
     setupShuffleboard();
+    m_TargetLastSeen = new Timer();
   }
   private void setupShuffleboard() {
     ShuffleboardTab vision = Shuffleboard.getTab("Vision");
@@ -104,31 +113,34 @@ public class ChaseTagCommand extends Command {
       var target = m_VisionSubsystem.getTargetForTag(TAG_TO_CHASE);
 
       if (target != null && !target.equals(lastTarget)) {
-          // This is new target data, so recalculate the goal
-          lastTarget = target;
+        // This is new target data, so recalculate the goal
+        lastTarget = target;
+        // restart our timer on fresh target data (start() method is no-op if alredy running)
+        m_TargetLastSeen.start();
+        m_TargetLastSeen.reset();
 
-          // Get the transformation from the camera to the tag (in 2d)
-          var camToTarget = target.getBestCameraToTarget();
-          var transform = new Transform2d(
-                  camToTarget.getTranslation().toTranslation2d(),
-                  camToTarget.getRotation().toRotation2d());  // not sure this 90 deg correctoin needed
+        // Get the transformation from the camera to the tag (in 2d)
+        var camToTarget = target.getBestCameraToTarget();
+        var transform = new Transform2d(
+            camToTarget.getTranslation().toTranslation2d(),
+            camToTarget.getRotation().toRotation2d());
 
-          // Transform the robot's pose to find the tag's pose
-          var cameraPose = robotPose.transformBy(CAMERA_TO_ROBOT.inverse());
-          Pose2d targetPose = cameraPose.transformBy(transform);
+        // Transform the robot's pose to find the tag's pose
+        var cameraPose = robotPose.transformBy(CAMERA_TO_ROBOT.inverse());
+        Pose2d targetPose = cameraPose.transformBy(transform);
 
-          // Transform the tag's pose to set our goal
-          goalPose = targetPose.transformBy(TAG_TO_GOAL);
-      }
+        // Transform the tag's pose to set our goal
+        goalPose = targetPose.transformBy(TAG_TO_GOAL);
 
-      if (goalPose != null) {
-          // Drive
+        if (goalPose != null) {
+          // only reset the goal when there is a new goal
           xController.setGoal(goalPose.getX());
           yController.setGoal(goalPose.getY());
           omegaController.setGoal(goalPose.getRotation().getRadians());
           m_xPub.set(goalPose.getX());
           m_yPub.set(goalPose.getY());
           m_omegaPub.set(goalPose.getRotation().getRadians());
+        }
       }
 
       double xSpeed = xController.calculate(robotPose.getX());
@@ -155,9 +167,23 @@ public class ChaseTagCommand extends Command {
       
   }
 
+  public boolean isFinished(){
+    if (xController.atGoal() && yController.atGoal() && omegaController.atGoal()) {
+      // if we're at the goal in all dimensions, we're done
+      System.out.println("Reached the Vision target goal - STOPPING");
+      return true;
+    }
+    if (m_TargetLastSeen.hasElapsed(STALE_TARGET_TIME)){
+      // If we haven't gotten new target data in a while we may have lost sight of it and should stop
+      System.out.println("Have not seen the target lately - STOPPING");
+      return true;
+    }
+    return false;
+  }
   @Override
   public void end(boolean interrupted) {
     drivetrainSubsystem.stopModules();
+    m_TargetLastSeen.stop();
   }
 
   public void initSendable(SendableBuilder builder) {
