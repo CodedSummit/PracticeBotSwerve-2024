dfa1779ce4eb6b6d1e1e5c7d1a5c31e23265a0c4
Cliff Stockdill <cstockdill@yahoo.com>
2026-01-28 10:40:16 -0700

    Update to use type-specific shooter speed entry for use with Elastic

--- src/main/java/frc/robot/subsystems/NoteShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/NoteShooterSubsystem.java
@@ -4,23 +4,15 @@
 
 package frc.robot.subsystems;
 
-import java.util.Map;
-
-import com.ctre.phoenix6.hardware.TalonFX;
-
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.Constants.ShooterConstants;
 
 /**
  * Models the mechanism that shoots Notes
@@ -39,8 +31,7 @@ public class NoteShooterSubsystem extends SubsystemBase {
 
 //  private TalonFX m_motor = new TalonFX(ShooterConstants.kShooterCanbusID, "rio");
   private SparkFlex m_motor = new SparkFlex(13, MotorType.kBrushless);
-  private double motorSpeed = ShooterConstants.kShooterSpeed;
-  private GenericEntry nt_shooterspeed;
+  private DoubleEntry nt_shooterspeed;
   /** Creates a new VisionSubsystem. */
   public NoteShooterSubsystem() {
     setupShuffleboard();
@@ -58,20 +49,19 @@ public class NoteShooterSubsystem extends SubsystemBase {
   }
 
   public double getSpeed(){
-    return nt_shooterspeed.getDouble(0.1);
+    return nt_shooterspeed.get(0.1);
+
   }
   public void spinUp() {
     // start the motor at some pre-defined constant speed
     m_motor.set(getSpeed());
   }
 
-  private void setupShuffleboard(){
-
-    ShuffleboardTab shootTab = Shuffleboard.getTab("shooter");
-    nt_shooterspeed = shootTab.addPersistent("ShooterSpeed",0.0)
-    .withWidget(BuiltInWidgets.kNumberSlider)
-    .withProperties(Map.of("min",-1.0,  "max",0.0))
-    .getEntry();
+  private void setupShuffleboard() {
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    NetworkTable table = inst.getTable("datatable");
+    nt_shooterspeed = table.getDoubleTopic("ShooterSpeed").getEntry(0.0);
+    nt_shooterspeed.set(0.0);
   }
   public void stop() {
     m_motor.set(0.0);
